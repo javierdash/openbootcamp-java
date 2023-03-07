d79015ee795d71e4245e56e289bca37e129d1335
Javier Herzkovich <javierdash@gmail.com>
2023-03-07 11:50:36 -0300

    entrega primer ejercicio de Java

---
+++ src/com/ejercicio1/PrimeraClase.java
@@ -0,0 +1,18 @@
+package com.ejercicio1;
+
+public class PrimeraClase {
+    public static void main(String[] args) {
+       int entero = 22;
+       long grande = 44959559;
+       double decimal = 3.3;
+       boolean esVerdadero = true;
+       String cadenaTexto = "hola primer ejercicio";
+
+        System.out.println("A continuación aparecen los tipos de datos más utilizados:");
+        System.out.println(entero);
+        System.out.println(grande);
+        System.out.println(decimal);
+        System.out.println(esVerdadero);
+        System.out.println(cadenaTexto);
+    }
+}
