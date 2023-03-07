2db95ac7d834bf11ace90dab54f503800f161fe3
Javier Herzkovich <javierdash@gmail.com>
2023-03-07 12:34:59 -0300

    entrega segundo ejercicio de Java

---
+++ src/com/ejercicio1/Entrega2.java
@@ -0,0 +1,13 @@
+package com.ejercicio1;
+
+import java.util.Scanner;
+
+public class Entrega2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el precio");
+         double precio = scanner.nextDouble();
+         double conIva = precio * .21 + precio;
+        System.out.println("El precio con IVA es $" + conIva);
+    }
+}
