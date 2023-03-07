9741780b6875f5d31700b57826a5c4c62d38327c
Javier Herzkovich <javierdash@gmail.com>
2023-03-07 13:15:08 -0300

    entrega tercer ejercicio de Java

---
+++ src/com/ejercicio1/Entrega3.java
@@ -0,0 +1,12 @@
+package com.ejercicio1;
+
+public class Entrega3 {
+    public static void main(String[] args) {
+        String[] nombres = {"Juan", "Pedro", "Javier", "Sandra", "Miguel"};
+
+        for(int i = 0; i < nombres.length; i++) {
+            System.out.println(nombres[i]);
+        }
+        
+    }
+}
